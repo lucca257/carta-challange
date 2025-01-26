de5f049fa4991c6ee6811881c2ce235c1b68f1c1
One Code Man <pedro.lucca@live.estacio.br>
2025-01-26 20:24:44 -0300

    feature: ordering api result

--- front/src/services/Vesting/vesting.service.js
+++ front/src/services/Vesting/vesting.service.js
@@ -7,15 +7,28 @@ export class VestingService {
      */
     static async getVestingSchedules() {
         const data = await ApiService.get("/mock-api/vesting");
-        return data.map(schedule => ({
-            label: schedule.label,
-            issueDate: schedule.issue_date,
-            vestingManager: schedule.vesting_manager_name,
-            vestingSchedule: schedule.vesting_schedule.map(entry => ({
-                date: entry.vesting_date,
-                amount: parseFloat(entry.amount),
-                cumulativeAmount: parseFloat(entry.cumulative_amount)
-            }))
-        }));
+        return data.map(schedule => {
+
+            const sortedVestingSchedule = schedule.vesting_schedule
+                .map(entry => ({
+                    date: new Date(entry.vesting_date),
+                    amount: parseFloat(entry.amount),
+                    cumulativeAmount: parseFloat(entry.cumulative_amount)
+                }))
+                .sort((a, b) => a.date - b.date);
+
+            let cumulativeAmount = 0;
+            sortedVestingSchedule.forEach(entry => {
+                cumulativeAmount += entry.amount;
+                entry.cumulativeAmount = cumulativeAmount;
+            });
+
+            return {
+                label: schedule.label,
+                issueDate: schedule.issue_date,
+                vestingManager: schedule.vesting_manager_name,
+                vestingSchedule: sortedVestingSchedule
+            };
+        });
     }
 }
